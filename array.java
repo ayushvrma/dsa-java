ac3098ea061971552ff75e6f18695691f0dc7595
Ayush <ayush8402@gmail.com>
2021-07-21 19:41:09 +0530

    added gitignore

---
+++ array.java
@@ -0,0 +1,12 @@
+class array
+{
+    public static void main(String[] args) {
+        int arr[] = new int[7];
+
+        for(int i=0; i< arr.length ; i++)
+        arr[i] = 2*i;
+
+        for(int i=0; i< arr.length ; i++)
+        System.out.println(arr[i]);
+    }
+}
