62c5038459ab48686395b1359ed733509ab0afc8
Ayush <ayush8402@gmail.com>
2021-07-25 16:11:54 +0530

    initialised integer linked list

---
+++ Lists/challenge/challenge2/EmployeeLinkedList.java
@@ -0,0 +1,51 @@
+package Lists.challenge.challenge2;
+public class EmployeeLinkedList{
+    private EmployeeNode head;
+    private int size;
+
+
+    public void addToFront(challenge2 employee)
+    {
+        EmployeeNode node = new EmployeeNode(employee);
+        node.setNext(head);
+        head = node;
+    }
+
+
+    public void printList(){
+        EmployeeNode current = head;
+        System.out.println("Head->");
+        while(current!=null)
+        {
+            System.out.print(current+" --> ");
+            current = current.getNext();
+        }
+        System.out.print("null");
+    }
+
+    public int getSize(){
+        return size;
+    }
+
+    public boolean isEmpty()
+    {
+        return head==null;
+    }
+
+    public EmployeeNode removeFromFront()
+    {
+        if(isEmpty())
+        return null;
+
+        EmployeeNode removedNode = head;
+        head = head.getNext();
+        size--;
+        removedNode.setNext(null); //to completly remove the node
+        return removedNode;
+    }
+
+    public void addSorted()
+    {
+        
+    }
+}
