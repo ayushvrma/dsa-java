9aafa568b3d65ca58ff5ac82d4a6feff48552add
Ayush <ayush8402@gmail.com>
2021-07-26 14:28:34 +0530

    notes on stacks

---
+++ stack/stack.java
@@ -0,0 +1,13 @@
+package stack;
+
+public class stack {
+    public static void main(String[] args) {
+        
+    }
+}
+
+/*
+Last in first out
+Ideal backing data structure: linked list
+eg: pile of notebooks
+*/
