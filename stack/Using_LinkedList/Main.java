cc95b1a09caf9615568b5fc5569a67892c54826b
Ayush <ayush8402@gmail.com>
2021-07-26 15:27:07 +0530

    completed Linked List implementation

--- stack/Using_LinkedList/Main.java
+++ stack/Using_LinkedList/Main.java
@@ -10,6 +10,18 @@ public class Main {
         Employee oyus = new Employee("oyus", "vrma", 120);
         Employee raghav = new Employee("raghav", "chnna", 120);
         
-        
+        LinkedStack stack = new LinkedStack();
+
+        stack.push(dkarla);
+        stack.push(unsa);
+        stack.push(oyus);
+        stack.push(raghav);
+
+        stack.pop();
+
+        System.out.println("Peeked item: "+stack.peek());
+
+        stack.printStack();
+
     }
 }
