14a81ccf34b8045ee881b0f97d34002f545e6d88
Ayush <ayush8402@gmail.com>
2021-07-26 14:50:35 +0530

    made new files

---
+++ stack/Using_Arrays/Main.java
@@ -0,0 +1,15 @@
+package stack.Using_Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        ArrayStack stack = new ArrayStack(10);
+
+        stack.push(new stack.Using_Arrays.Employee("dee", "karla", 120));
+        stack.push(new stack.Using_Arrays.Employee("unsa", "sechen", 120));
+        stack.push(new stack.Using_Arrays.Employee("oyus", "vrma", 120));
+        stack.push(new stack.Using_Arrays.Employee("raghav", "chnna", 120));
+    }
+    
+}
