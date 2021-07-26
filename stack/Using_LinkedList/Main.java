e8fa57140cc53680a95c61ec1d9aefa0d23bed85
Ayush <ayush8402@gmail.com>
2021-07-26 15:12:00 +0530

    initialised files

---
+++ stack/Using_LinkedList/Main.java
@@ -0,0 +1,15 @@
+package stack.Using_LinkedList;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        
+        Employee dkarla = new Employee("dee", "karla", 120);
+        Employee unsa = new Employee("unsa", "sechen", 120);
+        Employee oyus = new Employee("oyus", "vrma", 120);
+        Employee raghav = new Employee("raghav", "chnna", 120);
+        
+        
+    }
+}
