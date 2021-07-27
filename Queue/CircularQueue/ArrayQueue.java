d2f385c408d11dc3de1a86193ad2d0db057add03
Ayush <ayush8402@gmail.com>
2021-07-27 20:09:02 +0530

    modified array copy

---
+++ Queue/CircularQueue/ArrayQueue.java
@@ -0,0 +1,67 @@
+package Queue.CircularQueue;
+
+import java.util.NoSuchElementException;
+
+public class ArrayQueue {
+    private Employee queue[];
+    private int front;
+    private int back;
+
+    public ArrayQueue(int capacity)
+    {
+        queue = new Employee[capacity];
+    }
+
+    public void add (Employee employee)
+    {
+        if(size()==queue.length-1){
+            Employee newArray[] = new Employee[2*queue.length];
+            
+            System.arraycopy(queue, front, newArray, 0, queue.length-front); //to copy front to back/end of the queue
+            System.arraycopy(queue , 0, newArray, queue.length-front, back);
+
+            queue = newArray;
+        }
+        queue[back] =employee;
+        
+        if(back<queue.length-1)
+            back++;
+        else{
+            back=0;
+            }
+        }
+
+    public Employee remove(){
+        if(size()==0)
+        {
+            throw new NoSuchElementException();
+        }
+
+        Employee employee = queue[front];
+        queue[front] = null;
+        front++;
+
+        if(size()==0)
+        {
+            front=back=0;
+        }
+        return employee;
+    }
+
+    public int size()
+    {
+        return back-front;
+    }
+
+    public Employee peek()
+    {
+        if(size()==0)
+            throw new NoSuchElementException();
+        return queue[front];
+    }
+
+    public void printQueue(){
+        for(int i=front;i<back;i++)
+            System.out.println(queue[i]);
+    }
+}
