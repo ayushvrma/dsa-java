1d8cdc7aa31af3229596ed6ca09adc289e635257
Ayush <ayush8402@gmail.com>
2021-07-27 19:51:27 +0530

    completed linear queue

--- Queue/UsingArray/Main.java
+++ Queue/UsingArray/Main.java
@@ -3,6 +3,23 @@ package Queue.UsingArray;
 public class Main{
     public static void main(String[] args) {
         
+        Employee dkarla = new Employee("dee", "karla", 120);
+        Employee unsa = new Employee("unsa", "sechen", 120);
+        Employee oyus = new Employee("oyus", "vrma", 120);
+        Employee raghav = new Employee("raghav", "chnna", 120);
+
+        ArrayQueue queue = new ArrayQueue(10);
+        
+
+        queue.add(dkarla);
+        queue.add(unsa);
+        queue.add(oyus);
+        queue.add(raghav);
+
+        queue.remove();
+        System.out.println(queue.peek());
+
+        queue.printQueue();
     }
 }
 
