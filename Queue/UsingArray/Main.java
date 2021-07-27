962246bee58d16ed1b5a7ddd56a8694602ad56aa
Ayush <ayush8402@gmail.com>
2021-07-27 19:33:29 +0530

    added Employee.java

---
+++ Queue/UsingArray/Main.java
@@ -0,0 +1,15 @@
+package Queue.UsingArray;
+
+public class Main{
+    public static void main(String[] args) {
+        
+    }
+}
+
+/*
+Abstract datatype
+FIFO
+add/enqueue - adds item to the end of the list
+remove/dequeue - removes item from the start of the list
+peek- front of the list
+*/
