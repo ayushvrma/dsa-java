8c375d647d04e00e8088e98fc959222d9c20d32e
Ayush <ayush8402@gmail.com>
2021-07-27 18:05:51 +0530

    used different implementation

--- stack/Challenge/challenge.java
+++ stack/Challenge/challenge.java
@@ -1,12 +1,30 @@
 package stack.Challenge;
+import java.util.*;
 
 public class challenge {
     public static void main(String[] args) {
-        
+        System.out.println(isPalindrome("racecar"));
     }
 
-    public boolean isPalindrome(String input){
-        
-        return false;
+    public static boolean isPalindrome(String input){
+        input = input.toLowerCase();
+        String initial="";
+        String output = "";
+        LinkedList<Character> list = new LinkedList<>();
+        for(int i=0; i<input.length();i++)
+        {
+            if(Character.isLetter(input.charAt(i)))
+                list.add(input.charAt(i));
+                initial+= input.charAt(i);
+        }
+        for(int i=0; i<list.size();i++)
+        {
+            list2.add(list.pop());
+            output+=temp.pop();
+        }
+        if(initial==output)
+            return true;
+        else
+            return false;
     }
 }
