73ebaf3d2e914075489a3653d32c351301e299e4
Ayush <ayush8402@gmail.com>
2021-07-27 17:34:43 +0530

    initialised isPalindrome() method

--- Queue/Main.java
+++ Queue/Main.java
@@ -1,2 +1,7 @@
+package Queue;
+
 public class Main{
+    public static void main(String[] args) {
+        
+    }
 }
---
+++ stack/Challenge/challenge.java
@@ -0,0 +1,12 @@
+package stack.Challenge;
+
+public class challenge {
+    public static void main(String[] args) {
+        
+    }
+
+    public boolean isPalindrome(String input){
+        
+        return false;
+    }
+}
