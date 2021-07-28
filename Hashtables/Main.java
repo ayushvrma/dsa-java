3650698da25a86b983dd54cd111b8e982443eab8
Ayush <ayush8402@gmail.com>
2021-07-28 16:39:05 +0530

    added basic methods to hashtable

--- Hashtables/Main.java
+++ Hashtables/Main.java
@@ -2,7 +2,10 @@ package Hashtables;
 
 public class Main {
     public static void main(String[] args) {
-        
+        Employee dkarla = new Employee("dee", "karla", 120);
+        Employee unsa = new Employee("unsa", "sechen", 120);
+        Employee oyus = new Employee("oyus", "vrma", 120);
+        Employee raghav = new Employee("raghav", "chnna", 120);
     }
 }
 /*
---
+++ Hashtables/SimpleHashTable.java
@@ -0,0 +1,41 @@
+package Hashtables;
+
+public class SimpleHashTable{
+
+    private Employee[] hashtable;
+
+    public SimpleHashTable()
+    {
+        hashtable = new Employee[10];
+    }
+
+    private int hashKey(String key){
+        return key.length() % hashtable.length;
+    }
+
+    public void put(String key, Employee employee){
+        int hashedkey = hashKey(key);
+
+        if(hashtable[hashedkey] != null)
+        {
+            System.out.println("Already an employee at position "+ hashedkey);
+        }
+        else{
+            hashtable[hashedkey]=employee;
+        }
+    }
+
+    public Employee get(String key)
+    {
+        int hashedkey = hashKey(key);
+        return hashtable[hashedkey];
+    }
+
+    public void printTable()
+    {
+        for(int i=0 ;i<hashtable.length;i++ )
+        {
+            System.out.println(hashtable[i]);
+        }
+    }
+}
