94acaed640cf73923a15a50f79a4a983810ab37e
Ayush <ayush8402@gmail.com>
2021-07-28 16:24:15 +0530

    added hashtable notes

---
+++ Hashtables/Main.java
@@ -0,0 +1,13 @@
+package Hashtables;
+
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
+/*
+Abstract Data Type
+key value pairs- data types dont have to match
+Optimised for retrieval (when you know the key)
+Associative array is a type of hash table
+*/
