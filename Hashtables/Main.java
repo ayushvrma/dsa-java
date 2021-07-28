82b449d97cdbb20013adffbfd2ad32fd6d894cd0
Ayush <ayush8402@gmail.com>
2021-07-28 16:31:32 +0530

    added employee class

--- Hashtables/Main.java
+++ Hashtables/Main.java
@@ -10,4 +10,10 @@ Abstract Data Type
 key value pairs- data types dont have to match
 Optimised for retrieval (when you know the key)
 Associative array is a type of hash table
+
+To convert String keys to integers we use Object.hasCode() method - Hashing
+Collision occurs when more than one value has the same hash value
+
+Load factor- tells how much the table is full, can play role determining the time complexity for retrival
+eg - 0.4 (table 40% full)
 */
