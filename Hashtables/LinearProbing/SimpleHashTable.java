76680469efce1b6ec03e8c8ef36f32bf3a3d5285
Ayush <ayush8402@gmail.com>
2021-08-02 13:06:58 +0530

    implemented rehashing

--- Hashtables/LinearProbing/SimpleHashTable.java
+++ Hashtables/LinearProbing/SimpleHashTable.java
@@ -99,7 +99,18 @@ public class SimpleHashTable{
             return null;
         }
         Employee employee = hashtable[hashedKey].employee;
-        hashtable[hashedKey]= null;
+        //hashtable[hashedKey]= null;
+        
+        StoredEmployee[] oldHashtable = hashtable;
+        hashtable = new StoredEmployee[oldHashtable.length];
+
+        for(int i=0; i<oldHashtable.length;i++) //rehashing
+        {
+            if(oldHashtable[i]!=null)
+            {
+                put(oldHashtable[i].key, oldHashtable[i].employee);
+            }
+        }
         return employee;
     }
 }
