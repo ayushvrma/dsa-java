f7dfa4ff89a9859c58326c2e37de2e2dfa899899
Ayush <ayush8402@gmail.com>
2021-08-05 14:50:51 +0530

    put key and values to the HashMap

--- Hashtables/JDK/Main.java
+++ Hashtables/JDK/Main.java
@@ -1,9 +1,22 @@
 package Hashtables.JDK;
+import java.util.*;
 public class Main {
-    Employee dkarla = new Employee("dee", "karla", 120);
-    Employee unsa = new Employee("unsa", "sechen", 120);
-    Employee oyus = new Employee("oyus", "vrma", 120);
-    Employee raghav = new Employee("raghav", "chnna", 120);
+
+    public static void main(String[] args) {
+        
+        Employee dkarla = new Employee("dee", "karla", 120);
+        Employee unsa = new Employee("unsa", "sechen", 120);
+        Employee oyus = new Employee("oyus", "vrma", 120);
+        Employee raghav = new Employee("raghav", "chnna", 120);
+
+        Map<String, Employee> hashMap = new HashMap<String,Employee>();
+        hashMap.put("divij", dkarla);
+        hashMap.put("unnayan",unsa);
+        hashMap.put("ayush",oyus);
+        hashMap.put("raghav",raghav);
+
+        
+    }
 }
 
 /*
