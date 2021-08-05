8577b589a0fa66e84c2e33731060201f1d637988
Ayush <ayush8402@gmail.com>
2021-08-05 14:47:22 +0530

    added the employee class

--- Hashtables/JDK/Main.java
+++ Hashtables/JDK/Main.java
@@ -1,6 +1,9 @@
 package Hashtables.JDK;
 public class Main {
-    
+    Employee dkarla = new Employee("dee", "karla", 120);
+    Employee unsa = new Employee("unsa", "sechen", 120);
+    Employee oyus = new Employee("oyus", "vrma", 120);
+    Employee raghav = new Employee("raghav", "chnna", 120);
 }
 
 /*
