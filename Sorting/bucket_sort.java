7e684570c7fe05387ff0024896920944e10e3947
Ayush <ayush8402@gmail.com>
2021-08-05 23:01:53 +0530

    added bucketSort function

--- Sorting/bucket_sort.java
+++ Sorting/bucket_sort.java
@@ -1,7 +1,30 @@
 package Sorting;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class bucket_sort {
-    
+    public static void main(String[] args) {
+        int intArray[] = {54,46,83,66,95,92,43};
+
+    }
+
+    public static void bucketSort(int arr[])
+    {
+        List<Integer> buckets[] = new List[10];
+
+        for(int i=0; i<buckets.length;i++){
+            buckets[i] = new ArrayList<Integer>(); //linkedlist can also be used
+        }
+        for(int i=0;i<arr.length;i++)
+        {
+
+        }
+    }
+
+    private static int hash(int value){
+        return value/(int)10%10; //division will be done first cause of precedense
+    }
 }
 
 /*
