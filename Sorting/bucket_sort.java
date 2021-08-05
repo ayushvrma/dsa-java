2b4c848b9bb9e9cc0fd655d6807025b42e5f4609
Ayush <ayush8402@gmail.com>
2021-08-05 15:26:26 +0530

    theory of bucket sort

---
+++ Sorting/bucket_sort.java
@@ -0,0 +1,13 @@
+package Sorting;
+
+public class bucket_sort {
+    
+}
+
+/*
+uses hashing
+makes assumptions about the data
+can sort in O(n) time
+performs best when hashed values are evenly distributed (no collisions)
+its a generalisation of counting sort
+*/
