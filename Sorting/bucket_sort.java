91614a8ec346be097011cc8749c6d0f4d2491538
Ayush <ayush8402@gmail.com>
2021-08-05 23:18:28 +0530

    Collections.sort() not working

--- Sorting/bucket_sort.java
+++ Sorting/bucket_sort.java
@@ -1,11 +1,15 @@
 package Sorting;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import java.util.*;
 public class bucket_sort {
     public static void main(String[] args) {
         int intArray[] = {54,46,83,66,95,92,43};
+        bucketSort(intArray);
+
+        for(int i=0;i<intArray.length;i++)
+        {
+            System.out.println(intArray[i]);
+        }
 
     }
 
@@ -19,6 +23,16 @@ public class bucket_sort {
         for(int i=0;i<arr.length;i++)
         {
             buckets[hash(arr[i])].add(arr[i]);
+        }
+        for(List bucket:buckets){
+            Collections.sort(bucket); //uses merge sort
+        }
+        int j=0;
+        for(int i=0;i<buckets.length;i++)//i tracking where we are in the buckets array
+        {
+            for(int value:buckets[j]){
+                arr[j++]=value; //j is tracking where we are in the inputs array
+            }
         }
     }
 
