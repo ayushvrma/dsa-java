a92537857de51fd26f0bedaff96611ee7f034a21
Ayush <ayush8402@gmail.com>
2021-08-07 20:59:23 +0530

    added get method to TreeNode class

--- Searching/Trees/TreeNode.java
+++ Searching/Trees/TreeNode.java
@@ -42,6 +42,26 @@ public class TreeNode {
         }
     }
 
+    public TreeNode get(int value){
+        if(value==data)
+        {
+            return this; //return the current node (this node)
+        }
+        if(value<data)
+        {
+            if(leftChild!=null){
+                return leftChild.get(value);
+            }
+        }
+        else{
+            if(rightChild!=null)
+            {
+                return rightChild.get(value);
+            }
+        }
+        return null;
+    }
+
     public TreeNode(int data) {
         this.data = data;
     }
