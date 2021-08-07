0b73a386a81a8d233731dde769d1b216e04db6ed
Ayush <ayush8402@gmail.com>
2021-08-07 20:39:11 +0530

    initialised the tree

---
+++ Searching/Trees/TreeNode.java
@@ -0,0 +1,32 @@
+package Searching.Trees;
+
+public class TreeNode {
+    private int data;
+    private TreeNode leftChild;
+    private TreeNode rigNode;
+
+    public int getData() {
+        return this.data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public TreeNode getLeftChild() {
+        return this.leftChild;
+    }
+
+    public void setLeftChild(TreeNode leftChild) {
+        this.leftChild = leftChild;
+    }
+
+    public TreeNode getRigNode() {
+        return this.rigNode;
+    }
+
+    public void setRigNode(TreeNode rigNode) {
+        this.rigNode = rigNode;
+    }
+
+}
---
+++ Searching/Trees/bst.java
@@ -0,0 +1,11 @@
+package Searching.Trees;
+
+public class bst {
+    public static void main(String[] args) {
+        
+    }
+}
+/*
+can perform insertions, deletions and retrievals in O(log n) time
+left child has less value than parent, right has more
+*/
