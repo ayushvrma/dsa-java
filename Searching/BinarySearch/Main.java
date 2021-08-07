72179c90e86dad7f8b2c6b7e209669139bca3b01
Ayush <ayush8402@gmail.com>
2021-08-07 20:18:35 +0530

    added the reccursive function for binary search

--- Searching/BinarySearch/Main.java
+++ Searching/BinarySearch/Main.java
@@ -3,7 +3,8 @@ package Searching.BinarySearch;
 public class Main {
     public static void main(String[] args) {
         int arr[] = {-22,-15,1,7,20,35,55};
-        System.out.println("key is present at: " + binarySearch(arr, 20)+ " position");
+        //System.out.println("key is present at: " + binarySearch(arr, 20)+ " position");
+        System.out.println("key is present at: " + reccursiveBinarySearch(arr, 20, 0, arr.length-1) + " position");
     }
     public static int binarySearch(int arr[], int key)
     {
@@ -26,6 +27,24 @@ public class Main {
             }
         }
         return -1;
+    }
+
+    public static int reccursiveBinarySearch(int arr[], int key, int start, int end)
+    {
+        if(start>=end)
+        {
+            return -1;
+        }
+        int mid=(start+end)/2;
+        System.out.println(arr[mid]);
+        if (arr[mid] == key) {
+            return mid + 1;
+        } else if (arr[mid] < key) {
+            return reccursiveBinarySearch(arr, key, mid+1, end);
+        } else {
+            return reccursiveBinarySearch(arr, key, start, mid);
+        }
+
     }
 }
 //only on sorted arrays
