641aedb8d2d3b590bab20f650589331205a4cb26
Ayush <ayush8402@gmail.com>
2021-08-08 15:29:41 +0530

    initialised heap class

---
+++ Searching/Heaps/Main.java
@@ -0,0 +1,13 @@
+package Searching.Heaps;
+
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
+/*
+Heaps are binary trees where every parent is greater than or equal to its children (max heap)
+Heaps are complete trees
+children are added from left to right
+heapify: process of converting a binary tree into heap
+*/
