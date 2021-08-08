01aafed914aae52a47672c0e916868ebf6f36b93
Ayush <ayush8402@gmail.com>
2021-08-08 15:54:31 +0530

    added delete method

--- Searching/Heaps/Heap.java
+++ Searching/Heaps/Heap.java
@@ -28,6 +28,59 @@ public class Heap {
         return 2* index + (left?1:2); //if its left we want 1 right we want 2 (formula)
     }
 
+    public int delete(int index){ //usuallyu done with root
+        if(isEmpty()){
+            throw new IndexOutOfBoundsException("heap is empty");
+        }
+        int parent = getParent(index);
+        int deletedValue = heap[index];
+
+        heap[index] = heap[size-1]; //right most value will replace whatever is at the index
+
+        if(index ==0 || heap[index]<heap[parent]){
+            fixHeapBelow(index, size-1);
+         }
+         else{
+             fixHeapAbove(index);
+         }
+         size--;
+
+         return deletedValue;
+
+    }
+
+    private void fixHeapBelow(int index, int lastHeapIndex){
+        int childToSwap;
+
+        while(index<=lastHeapIndex){
+            int leftChild = getChild(index, true);
+            int rightChild =getChild(index, false);
+
+            if(leftChild<= lastHeapIndex){
+                if(rightChild> lastHeapIndex) //means it does not have a right child, means we want to swap the left child
+                {
+                    childToSwap = leftChild;
+                }
+                else{
+                    childToSwap = (heap[leftChild]> heap[rightChild])?leftChild:rightChild; //assigning with the child having the greatest value
+                }
+                if(heap[index]<heap[childToSwap]){
+                    int temp = heap[index];
+                    heap[index] = heap[childToSwap];
+                    heap[childToSwap] = temp;
+            }
+            else{//if the value is already greater than its child then we are good to go
+                break;
+            }
+            index = childToSwap;
+
+            }
+            else{
+                break;
+            }
+        }
+    }
+
     private void fixHeapAbove (int index){
         int newValue = heap[index];
         while(index>0 && newValue> heap[getParent(index)]){//means weve hit the root
